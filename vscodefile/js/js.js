fb5f9b7921346c54705f0ca81793eb061de03437
rinatraev <64296176+rinatraev@users.noreply.github.com>
2021-10-27 14:48:03 +0500

    Add files via upload

---
+++ vscodefile/js/js.js
@@ -0,0 +1,10 @@
+"use strict"
+document.write("</br>JS connected");
+let name = "John";
+function show (){
+    name = "Rinat";
+    document.write("</br>" + name);
+}
+document.write("</br>" + name);
+show();
+document.write("</br>" + name);
